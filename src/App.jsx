2956996786291c88a9e7159778438aa73c00871d
Zulrahman Asmurad <125336231+Zulrahman92@users.noreply.github.com>
2023-08-05 16:39:14 +0800

    add modules

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import kamusStail from "./App.module.css";
+
+function App() {
+
+  return (
+    <>
+    <p className={[kamusStail.title]}>Hello React</p>
+    <p className={[kamusStail.title2]}>Hello React</p>
+
+    </>
+  )
+}
+
+export default App
